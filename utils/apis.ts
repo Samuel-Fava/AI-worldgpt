211e2ed68ea04907aaa65d7d08fd7832b0e40650
Samuel <ozym73554@gmail.com>
2025-07-12 05:29:10 +0900

    git api router

--- utils/apis.ts
+++ utils/apis.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const aipRoute = () => {
     return axios.create({
-        baseURL: https://worldgpt.up.railway.app,
+        baseURL: "https://worldgpt.up.railway.app",
         headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${localStorage.getItem("token") || ""}`,
